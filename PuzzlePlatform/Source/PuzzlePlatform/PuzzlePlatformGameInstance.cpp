3960960438f39fbc0e30a83b140d71f4d696cbfb
abolfazlplp <abolfazlplp@gmail.com>
2022-03-04 21:17:10 +0330

    Server and Join Command

--- PuzzlePlatform/Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
+++ PuzzlePlatform/Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
@@ -3,3 +3,22 @@
 
 #include "PuzzlePlatformGameInstance.h"
 
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer) {
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor!"));
+}
+
+void UPuzzlePlatformGameInstance::Init() {
+	UE_LOG(LogTemp, Warning, TEXT("init"));
+}
+
+void UPuzzlePlatformGameInstance::Host() {
+	GetEngine()->AddOnScreenDebugMessage(0, 2, FColor().Green, TEXT("Hosting"));
+
+	GetWorld()->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+}
+
+void UPuzzlePlatformGameInstance::Join(const FString& Address) {
+	GetEngine()->AddOnScreenDebugMessage(0, 4, FColor().Green,FString::Printf(TEXT("Join %s"), *Address));
+
+	GetFirstLocalPlayerController()->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
